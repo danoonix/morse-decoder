8e79da0540bfdef2db20726d9137ef5fdaec71bb
Daria Trubnikova <valkrelex@gmail.com>
2023-01-27 11:35:27 +0300

    feat: add morse-decoder decision

--- src/index.js
+++ src/index.js
@@ -38,7 +38,32 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const space = '**********';
+    const dot = '10';
+    const dash = '11';
+    let res = '';
+    for(let i = 0; i < expr.length; i = i + 10) {
+        let resStr = '';
+        let str = expr.slice(i, i + 10);
+        if(str === space) {
+            res = res + ' ';
+            continue;
+        } else {
+            str = (+(str / 1)).toString();
+        }
+        for(let j = 0; j < str.length; j = j + 2) {
+            let strPart = str.slice(j, j+2);
+            if(strPart === dot) {
+                resStr = resStr + '.';
+            }
+            if(strPart === dash) {
+                resStr = resStr + '-';
+            }
+        }
+        res += MORSE_TABLE[resStr];
+        resStr = '';
+    }
+    return res;
 }
 
 module.exports = {
